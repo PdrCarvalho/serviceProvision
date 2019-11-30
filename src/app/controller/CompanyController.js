35befb46ae74448f3bde80e4fd464305f4abdb96
Mateus Santiago <mateus.santiago01@gmail.com>
2019-11-30 17:37:23 -0300

    Alterações de code style

--- src/app/controller/CompanyController.js
+++ src/app/controller/CompanyController.js
@@ -1,5 +1,6 @@
 import Company from '../model/company'
 import * as Yup from 'yup'
+
 class CompanyController {
     async Store(req, res) {
         const schema = Yup.object().shape({
@@ -8,16 +9,21 @@ class CompanyController {
             password: Yup.string().required().min(3),
             service: Yup.string().required()
         });
+
         if (!(await schema.isValid(req.body))) {
-            return res.status(400).json({ error: 'Validation fails' });
+            return res.status(400).json({ error: "Validation fails" });
         }
+
         const{name,email,password,service,description} = req.body
-        const company = await Company.create({name,email,password,service,description})
+        const company = await Company.create({name, email, password, service, description})
+        
         return res.json(company)
     }
-    async FindAll(req, res){
+    
+    async FindAll(req, res) {
         const companys  = await Company.find();
         res.json(companys);
     }
 }
+
 export default new CompanyController();
